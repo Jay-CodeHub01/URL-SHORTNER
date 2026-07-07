d4963723a2395359dca748648f671a597d089941
Jay Patel <pateljay18v@gmail.com>
2026-07-07 15:44:34 +0530

    Add errorHandler

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import connectDB from './src/config/mongo.config.js';
 
 import shortUrl from './src/routes/shorturl.route.js';
 import { redirectToFullUrl } from './src/controller/shorturl.controller.js';
+import errorHandler from './src/middleware/errorHandler.js';
 
 dotenv.config();
 
@@ -13,8 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/create', shortUrl);
-
 app.get('/:id', redirectToFullUrl);
+
+app.use(errorHandler);
+
 
 app.listen(3000 , ()=> {
     connectDB();
