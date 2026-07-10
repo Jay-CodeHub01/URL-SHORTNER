af1eaea8149a80f838d79eade885d535bc0703a0
Jay Patel <pateljay18v@gmail.com>
2026-07-10 15:23:01 +0530

    Create userModel

---
+++ server/src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  avatar: {
+    type: String,
+    required: false,
+    default: "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp",
+  },
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
