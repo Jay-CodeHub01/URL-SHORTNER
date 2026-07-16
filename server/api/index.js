7762b51f6f54d9b32cb7e078857d082c67549361
Jay Patel <pateljay18v@gmail.com>
2026-07-16 19:17:11 +0530

    fix db issue

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,7 @@
 import app from "../app.js"
+import connectDB from "../src/config/mongo.config.js"
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  await connectDB()
   return app(req, res)
 }
